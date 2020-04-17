d0db8c04214a180c8f14ed4390fa3cff82d4ef21
Christopher Rogers <chris@MacBook-Pro.local>
2020-04-17 13:34:17 +0100

    almost working

---
+++ appsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 120
+node seeds/seed.js
+node app.js
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Starting replica set initialize"
+until mongo --host mongodb://db --eval "print(\"waited for connection\")"
+do
+    sleep 60
+done
+mongo mongodb://db <<EOF
+var cfg = {
+    "_id": "rs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 0,
+            "host": "db:27017",
+            "priority": 2
+        },
+        {
+            "_id": 1,
+            "host": "db1:27017",
+            "priority": 0
+        },
+        {
+            "_id": 2,
+            "host": "db2:27017",
+            "priority": 0
+        }
+    ],settings: {chainingAllowed: true}
+};
+rs.initiate(cfg, { force: true });
+rs.reconfig(cfg, { force: true });
+rs.slaveOk();
+db.getMongo().setReadPref('nearest');
+db.getMongo().setSlaveOk();
+EOF
